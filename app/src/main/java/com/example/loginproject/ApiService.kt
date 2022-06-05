b35bc4385bb84a0f6113b35a311e60952b46eb37
minseo <msp05070478@gmail.com>
2022-06-05 18:19:50 +0900

    final commit

--- app/src/main/java/com/example/loginproject/ApiService.kt
+++ app/src/main/java/com/example/loginproject/ApiService.kt
@@ -3,12 +3,13 @@ package com.example.loginproject
 import retrofit2.Call
 import retrofit2.http.Body
 import retrofit2.http.POST
+import retrofit2.http.PUT
 
 interface ApiService {
 
     @POST("auth/register/")
     fun getSignUp(@Body signUpRequest: SignUpRequest) : Call<SignUpResponse>
 
-    @POST("auth/login/")
+    @PUT("auth/login/")
     fun getSignIn(@Body signInRequest: SignInRequest) : Call<SignInResponse>
 }
